e67209c9996c902951cd7a71aae1fcb09708b490
Simone Pelagatti <simone.pelagatti01@gmail.com>
2017-11-23 14:51:05 +0100

    commented scripts

--- scraping/trovaprezziScraper.php
+++ scraping/trovaprezziScraper.php
@@ -1,15 +1,31 @@
 <?php
+// Includo la libreria necessaria allo scraping della DOM
 require_once("simple_html_dom.php");
 
+// Elenco di prodotti di cui voglio ricercare il prezzo migliore
 $items = ["iphone 7", "samsung s8", "playstation 4 pro", "asus zenbook"];
 
 foreach ($items as $item) {
 
 	echo "Oggetto: ".$item."\n";
 
-	$response = file_get_contents("https://www.trovaprezzi.it/categoria.aspx?id=-1&libera=".str_replace(" ", "+", $item));
+	/***
+	*	La URL utilizzata di seguito è stata trovata analizzando la network activity da Chrome
+	***/
+	$searchUrl = "https://www.trovaprezzi.it/categoria.aspx?id=-1&libera=";
+
+	// Costruisco l'URL per ricercare l'oggetto analizzato
+	$response = file_get_contents($searchUrl.str_replace(" ", "+", $item));
+	// https://www.trovaprezzi.it/categoria.aspx?id=-1&libera=iphone+7
 	$domResponse = new \simple_html_dom($response);
 
+	/***
+	*	Posso ottenere due tipi di risposta:
+	*		1- Se il nome del prodotto è il nome esatto allora ottengo la pagina del prodotto in questione
+	*		2- Se il nome del prodotto è parziale allora ottengo l'elenco dei prodotti disponibili
+	***/
+
+	// Caso 1: prendo il prezzo migliore del prodotto
 	if($domResponse->find(".catsMI a", 0)){
 		$itemPageLink = $domResponse->find(".catsMI a", 0)->href;
 		$response = file_get_contents("https://www.trovaprezzi.it".$itemPageLink);
@@ -18,6 +34,7 @@ foreach ($items as $item) {
 		echo trim($domResponse->find(".listing_item .item_basic_price", 0)->plaintext)."\n";
 	}
 
+	// Caso 2: prendo tutti i prodotti possibili e il loro prezzo migliore
 	if($domResponse->find(".product_best_prices")){
 		foreach ($domResponse->find(".product_best_prices") as $product) {
 			echo $product->find("h2", 0)->plaintext."\t";
@@ -27,6 +44,7 @@ foreach ($items as $item) {
 
 	echo "\n---------------------------------------------------------------------\n";
 
+	// Attendo 1 secondo prima di effettuare nuove chiamate
 	sleep(1);
 
 }
--- scraping/websiteContentScraper.php
+++ scraping/websiteContentScraper.php
@@ -1,17 +1,24 @@
 <?php
+// includo la libreria necessaria allo scraping della DOM
 require_once("simple_html_dom.php");
 
+// elenco di siti web su cui effetuare lo scraping
 $websites = ["https://www.instilla.it", "https://www.primisullemappe.it", "https://www.rocketshare.io/", "https://lacerba.io"];
 
 foreach ($websites as $website) {
 
 	echo "Website: ".$website."\n";
+
+	// Download del pagesource della pagina
 	$websiteSource = file_get_contents($website);
 
+	// Analizzo il pagesource come DOM
 	$websiteDom = new \simple_html_dom($websiteSource);
+	// Estraggo il primo H1 presente nel sito web
 	echo "\nH1: ".$websiteDom->find("h1", 0)->plaintext."\n";
 
 	echo "\nRicerca tramite RegEx:\n";
+	// RegEx PHP per trovare stringhe particolari nel pagesource della pagina
 	$pattern = '/(https:\/\/www.googletagmanager.com\/gtm.js|https:\/\/www.facebook.com\/tr\?id=|\/\/www.google-analytics.com\/analytics.js)/';
 	preg_match_all($pattern, $websiteSource, $matches);
 	foreach ($matches[0] as $match) {
@@ -19,6 +26,7 @@ foreach ($websites as $website) {
 	}
 
 	echo "\nRicerca di una stringa:\n";
+	// Parola che voglio ricercare all'interno del pagesource della pagina
 	$searchString = "marketing";
 	if(strpos($websiteSource, $searchString)){
 		echo $searchString." TROVATO\n";
@@ -28,5 +36,6 @@ foreach ($websites as $website) {
 
 	echo "\n---------------------------------------------------------------------\n";
 
+	// Attendo 1 secondo prima di effettuare nuove chiamate
 	sleep(1);
 }
